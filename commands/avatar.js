24073feff3c06696fed63a85076ca21bff116c80
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-02-03 17:23:20 -0500

    Created avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "avatar",
+    description: "Shows the user's pfp.",
+    aliases: ["av", "icon", "pfp"],
+    execute(client, message, args) {
+        if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`;
+		});
+
+		message.channel.send(avatarList);
+    }
+}
