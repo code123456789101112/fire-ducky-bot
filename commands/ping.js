b43b827614181a69ef6842966854c2abe2cb3fcd
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-02-03 17:11:03 -0500

    Changed Cooldowns

--- commands/ping.js
+++ commands/ping.js
@@ -1,23 +1,11 @@
-const talkedRecently = new Set();
-
 module.exports = {
 	name: 'ping',
 	description: 'Shows the bot\'s ping.',
-	cooldown: "10",
+	cooldown: 10,
 	execute(client, message, args) {
-		if (talkedRecently.has(message.author.id)) {
-            message.channel.send("Wait 10 seconds before getting typing this again. - " + message.author.username);
-		} else {
-			message.channel.send("Pinging...").then(m =>{
-				var ping = m.createdTimestamp - message.createdTimestamp;
-				m.edit(`Pong! \`${ping}ms\``);
-			});
-
-			talkedRecently.add(message.author.id);
-			setTimeout(() => {
-
-			talkedRecently.delete(message.author.id);
-			}, 10000);
-		}
-	},
-};
+		message.channel.send("Pinging...").then(m =>{
+			var ping = m.createdTimestamp - message.createdTimestamp;
+			m.edit(`Pong! \`${ping}ms\``);
+		});
+	}
+}
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, token, ownerID, botID } = require('./config.json');
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, token, ownerID, botID } = require("./config.json");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -14,8 +14,10 @@ for (const file of commandFiles) {
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setActivity('Nobody', { type: 'LISTENING' });
+	client.user.setActivity("Nobody", { type: "LISTENING" });
 });
+
+const cooldowns = new Discord.Collection();
 
 client.on("guildMemberAdd", (member) => {
 	const welcomeChannel = member.guild.channels.cache.get("801125577644834913");
@@ -27,7 +29,7 @@ client.on("guildMemberRemove", (member) => {
 	welcomeChannel.send(`${member} has just left the server. :(`);
 });
 
-client.on('message', message => {
+client.on("message", message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -37,22 +39,42 @@ client.on('message', message => {
 
 	if (!command) return;
 
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.reply('I can\'t execute that command inside DMs!');
+	if (command.guildOnly && message.channel.type === "dm") {
+		return message.reply("I can\"t execute that command inside DMs!");
 	}
 
 	if (command.permissions) {
 		const authorPerms = message.channel.permissionsFor(message.author);
 		if (!authorPerms || !authorPerms.has(command.permissions)) {
-			return message.reply('You can not do this!');
+			return message.reply("You can not do this!");
 		}
 	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 	try {
 		command.execute(client, message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.reply("There was an error trying to execute that command!");
 	}
 });
 
