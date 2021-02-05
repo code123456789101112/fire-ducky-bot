04e0705c2ca31207b6478d67c4c15b7bf8a13d7e
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-02-05 10:06:01 -0500

    Fixed Lockdown

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -7,13 +7,18 @@ module.exports = {
     description: "Stops new members from joining.",
     execute(client, message, args) {
         if (enabled === true) {
+            message.channel.send("Lockdown disabled.");
             enabled = false;
-            return message.channel.send("Lockdown disabled.");
+        } else if (enabled === false) {
+            message.channel.send("Server is locked down. No new members can join");
+            enabled = true;
         }
-        
-        message.channel.send("Server is locked down. No new members can join");
-        enabled = true;
+
         client.on("guildMemberAdd", (member) => { 
+            if (enabled === false) {
+                return;
+            }
+            
             member.send("Sorry, but this server in currently on lockdown.");
             member.kick();
         });
