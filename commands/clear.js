0b7fc6c6006e942f5ad580866a539c21c7df06bb
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-02-09 14:21:22 -0500

    Created clear.js

---
+++ commands/clear.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "clear",
+    usage: "number-of-messages",
+    permissions: "ADMINISTRATOR",
+    aliases: ["purge", "c"],
+    description: "Clears specified number of messages in a channel.",
+    async execute(client, message, args) {
+        if (isNaN(args[0])) return message.channel.send("You didn't say how many messages to delete!");
+
+        await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
+            message.channel.bulkDelete(messages);
+        });
+    }
+};
