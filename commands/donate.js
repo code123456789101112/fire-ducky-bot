059c79e93b0dba85ab8d22693001abdd3e118599
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-02-10 09:16:34 -0500

    Created donate.js

---
+++ commands/donate.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "donate",
+    description: "Donate to giveaway or heist!",
+    cooldown: "3600",
+    execute(client, message, args) {
+        let gawtype;
+        let amount;
+       
+        message.channel.send("Do you want to donate to a giveaway or a heist?");
+        message.channel.awaitMessages(m => m.author.id == message.author.id,
+            {max: 1, time: 30000}).then(collected => {
+                if (collected.first().content === "giveaway") {
+                    gawtype = "giveaway";
+                    message.channel.send("What is your giveaway for? Explain everything including amount, time, amount of winners, etc.");
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        {max: 1, time: 60000}).then(collected => {
+                            amount = collected.first().content;
+                            const embed = new Discord.MessageEmbed()
+                                .setTitle(`${message.author.username}'s donation`)
+                                .setDescription(`${message.author} wants to donate to a ${gawtype}: ${amount}!`)
+                                .setColor("#ff0000")
+                                .setThumbnail("https://media.discordapp.net/attachments/781155105063043082/801151243987714058/fire_breathing_rubber_duckies.jpg?width=412&height=412")
+                            message.channel.send(`<@&801147084732497971> - <@${message.author.id}>`);
+                            return message.channel.send(embed);
+                        }).catch(() => {
+                            return message.channel.send("You have not responded, cancelling donation.");
+                        });
+                } else if (collected.first().content === "heist") {
+                    gawtype = "heist";
+                    message.channel.send("How much should the heist be? It can't be too much because of limited bank space and can't be too little because then it's not fun.");
+                    message.channel.awaitMessages(m => m.author.id == message.author.id,
+                        {max: 1, time: 60000}).then(collected => {
+                            amount = collected.first().content;
+                            const embed = new Discord.MessageEmbed()
+                                .setTitle(`${message.author.username}'s donation`)
+                                .setDescription(`${message.author} wants to donate to a ${gawtype}: ${amount}!`)
+                                .setColor("#ff0000")
+                                .setThumbnail("https://media.discordapp.net/attachments/781155105063043082/801151243987714058/fire_breathing_rubber_duckies.jpg?width=412&height=412")
+                            message.channel.send(`<@&801147084732497971> - <@${message.author.id}>`);
+                            return message.channel.send(embed);
+                        }).catch(() => {
+                            return message.channel.send("You have not responded, cancelling donation.");
+                        });
+                } else {
+                    return message.channel.send("That's not a valid donation type, cancelling donation.");
+                }
+            
+            }).catch(() => {
+                return message.channel.send("You have not responded, cancelling donation.");
+            });
+    }
+};
