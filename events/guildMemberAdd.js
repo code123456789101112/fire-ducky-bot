0d3e02f7ebc5bf3a74419b8b763579eaae11e504
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-03-18 09:11:21 -0400

    Commit

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ module.exports = (client, member) => {
     const welcomeChannel = member.guild.channels.cache.get("801125577644834913");
 	welcomeChannel.send(`Please welcome <@${member.id}> to the server!`);
 
-	if (member.bot) {
+	if (member.user.bot) {
 		const role = member.guild.roles.cache.get("801149263907258449");
 		member.roles.add(role);
 	} else {
