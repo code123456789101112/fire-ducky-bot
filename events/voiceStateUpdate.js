6764cc4025e760d7d75042e8faeeac3cb5c21b69
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-03-20 08:42:34 -0400

    Commit

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,15 @@
+module.exports = (client, oldState, newState) => {
+    if (!client.l2l) return;
+
+    if (client.wr && newState.channelID === client.wr.id) {
+        newState.member.voice.setChannel(client.ch);
+    } else if (newState.channelID === client.ch.id && client.ch.members.size == 1 && !client.wr) {
+        console.log(client.ch.id);
+        client.channels.cache.get("801148810725949471").send(`${newState.member} IS THE WINNER OF THE LAST TO LEAVE VC EVENT!!!`);
+
+        client.ch.delete();
+        delete client.l2l;
+    } else if (oldState.channel && !newState.channel && oldState.channelID === client.ch.id && !client.wr) {
+        client.chlogs.send(`${oldState.member.user.tag} HAS LEFT THE VC, THEY ARE OUT!!!!! ${client.ch.members.size} REMAIN!!!!!!!!`);
+    }
+};   
