e93e55fbcada9802a70cefef140d8b400c972d55
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-05-02 09:42:14 -0400

    Commit

--- structs/loadDirs.ts
+++ structs/loadDirs.ts
@@ -3,28 +3,31 @@ import Client from "./client.js"
 
 export default {
     async loadEvents(client: Client) {
-        const files = fs.readdirSync("./events");
+        const files: string[] = fs.readdirSync("./events");
         for (const file of files) {
             const event = (await import(`../events/${file.replace(".ts", ".js")}`)).default;
 
-            const name = file.split(".")[0];
+            const name: string = file.split(".")[0];
             client.on(name, event.bind(null, client));
 
             console.log(`Loaded ${name} event.`);
         }
+        
+        console.log();
     },
 
     async loadCommands(client: Client) {
-        const folders = fs.readdirSync("./commands/");
+        const folders: string[] = fs.readdirSync("./commands/");
         for (const folder of folders) {
-            const files = fs.readdirSync(`./commands/${folder}/`);
+            const files: string[] = fs.readdirSync(`./commands/${folder}/`);
             for (const file of files) {
                 const command = (await import(`../commands/${folder}/${file.replace(".ts", ".js")}`)).default;
-                const name = file.split(".")[0];
-                client.commands.set(name, command);
+                client.commands.set(command.name, command);
 
-                console.log(`Loaded ${name} command.`);
+                console.log(`Loaded ${command.name} command.`);
             }
+
+            console.log();
         }
     }
 }
