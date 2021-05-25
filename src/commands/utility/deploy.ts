a8b1d6e98a262822454773cf07fed2031018b195
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-05-25 08:48:13 -0400

    Commit

--- src/commands/utility/deploy.ts
+++ src/commands/utility/deploy.ts
@@ -2,16 +2,17 @@ import Message from "../../structs/message.js";
 import Client from "../../structs/client.js";
 
 import Command from "../../structs/command.js";
+import { ApplicationCommandData } from "discord.js";
 
 export default new Command({
     name: "deploy",
-    description: "registers slash commands",
+    description: "Registers slash commands.",
     devOnly: true,
     async execute(client: Client, message: Message) {
         if (!client.application?.owner) await client.application?.fetch();
 
-        const commandInfo = client.slashCommands.map(command => command.data);
-        await client.application?.commands.set(commandInfo);
+        const commandInfo: ApplicationCommandData[] = client.slashCommands.map(command => command.data);
+        await message.guild?.commands.set(commandInfo);
 
         message.reply("done");
     }
