7b89fe7ac04c11e3cb8e05452b9f31e6ec13e7ac
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-05-24 21:33:39 -0400

    Commit

--- src/commands/utility/deploy.ts
+++ src/commands/utility/deploy.ts
@@ -1,8 +1,6 @@
 import Message from "../../structs/message.js";
 import Client from "../../structs/client.js";
 
-import fs from "fs";
-
 import Command from "../../structs/command.js";
 
 export default new Command({
@@ -12,10 +10,8 @@ export default new Command({
     async execute(client: Client, message: Message) {
         if (!client.application?.owner) await client.application?.fetch();
 
-        const commandFiles = fs.readdirSync("./dist/src/slashCommands/");
-        const commandInfo = await Promise.all(commandFiles.map(async file => (await import(`../../slashCommands/${file}`)).default.info));
-
-        client.application?.commands.set(commandInfo);
+        const commandInfo = client.slashCommands.map(command => command.info);
+        await client.application?.commands.set(commandInfo);
 
         message.reply("done");
     }
---
+++ src/slashCommands/donate.ts
@@ -0,0 +1,60 @@
+import { CommandInteraction, CommandInteractionOption, MessageEmbed } from "discord.js";
+import { SlashCommand } from "../structs/command.js";
+
+import Client from "../structs/client.js";
+
+export default new SlashCommand({
+    info: {
+        name: "donate",
+        description: "Donation command.",
+        options: [{
+            name: "time",
+            description: "The duration of the giveaway.",
+            type: "STRING",
+            required: true
+        }, {
+            name: "amount-of-winners",
+            description: "The amount of people that will win.",
+            type: "INTEGER",
+            required: true
+        }, {
+            name: "req",
+            description: "The requirement(s)/request(s) for the giveaway.",
+            type: "STRING",
+            required: true
+        }, {
+            name: "prize",
+            description: "The prize of course.",
+            type: "STRING",
+            required: true
+        }, {
+            name: "message",
+            description: "Your message for the giveaway.",
+            type: "STRING",
+            required: false
+        }]
+
+    },
+    async execute(_client: Client, interaction: CommandInteraction, args: CommandInteractionOption[]) {
+        const embed: MessageEmbed = new MessageEmbed()
+            .setTitle(`${interaction.user.tag} wants to donate to a giveaway!!`)
+            .addFields({
+                name: "Time",
+                value: args[0].value as string
+            }, {
+                name: "Amount Of Winners:",
+                value: args[1].value as string
+            }, {
+                name: "Requirement(s):",
+                value: args[2].value as string
+            }, {
+                name: "Prize:",
+                value: args[3].value as string
+            })
+            .setColor("#00ff00")
+            .setFooter("Thank you for your kind donation!!");
+        if (args[4]?.value) embed.addField("Message:", args[4].value as string);
+            
+        await interaction.reply(embed);
+    }   
+});
