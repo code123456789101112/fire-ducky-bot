02d167dd18c24bb390f1e2c531ea154faaaa8d2e
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-05-25 10:26:14 -0400

    Commit

--- src/slashCommands/donate.ts
+++ src/slashCommands/donate.ts
@@ -8,57 +8,94 @@ export default new SlashCommand({
         name: "donate",
         description: "Donation command.",
         options: [{
-            name: "time",
-            description: "The duration of the giveaway.",
-            type: "STRING",
-            required: true
+            name: "giveaway",
+            description: "Donate to a giveaway.",
+            type: "SUB_COMMAND",
+            options: [{
+                name: "time",
+                description: "The duration of the giveaway.",
+                type: "STRING",
+                required: true
+            }, {
+                name: "amount-of-winners",
+                description: "The amount of people that will win.",
+                type: "INTEGER",
+                required: true
+            }, {
+                name: "req",
+                description: "The requirement(s)/request(s) for the giveaway.",
+                type: "STRING",
+                required: true
+            }, {
+                name: "prize",
+                description: "The prize of course.",
+                type: "STRING",
+                required: true
+            }, {
+                name: "message",
+                description: "Your message for the giveaway.",
+                type: "STRING",
+                required: false
+            }]
         }, {
-            name: "amount-of-winners",
-            description: "The amount of people that will win.",
-            type: "INTEGER",
-            required: true
-        }, {
-            name: "req",
-            description: "The requirement(s)/request(s) for the giveaway.",
-            type: "STRING",
-            required: true
-        }, {
-            name: "prize",
-            description: "The prize of course.",
-            type: "STRING",
-            required: true
-        }, {
-            name: "message",
-            description: "Your message for the giveaway.",
-            type: "STRING",
-            required: false
+            name: "heist",
+            description: "Donate to a heist.",
+            type: "SUB_COMMAND",
+            options: [{
+                name: "amount",
+                description: "The amount the heist should be.",
+                type: "INTEGER",
+                required: true
+            }, {
+                name: "message",
+                description: "Your message for the heist.",
+                type: "STRING",
+                required: false
+            }]
         }]
-
     },
-    cooldown: 1800,
+    cooldown: 600,
     async execute(client: Client, interaction: CommandInteraction, args: CommandInteractionOption[]) {
-        const embed: MessageEmbed = new MessageEmbed()
-            .setTitle(`${interaction.user.tag} wants to donate to a giveaway!!`)
-            .addFields({
-                name: "Time",
-                value: args[0].value as string
-            }, {
-                name: "Amount Of Winners:",
-                value: args[1].value as string
-            }, {
-                name: "Requirement(s):",
-                value: args[2].value as string
-            }, {
-                name: "Prize:",
-                value: args[3].value as string
-            })
-            .setColor("#00ff00")
-            .setFooter("Thank you for your kind donation!!");
-        if (args[4]?.value) embed.addField("Message:", args[4].value as string);
-            
-        await interaction.reply(`<@&${client.config.ids.roles.giveaway}>`, {
-            embeds: [embed],
-            allowedMentions: { parse: ["roles", "users", "everyone"] }
-        });
+        if (args[0].name === "giveaway") {
+            const options = args[0].options as CommandInteractionOption[];
+
+            const embed: MessageEmbed = new MessageEmbed()
+                .setTitle(`${interaction.user.tag} wants to donate to a giveaway!!`)
+                .addFields({
+                    name: "Time",
+                    value: options[0].value as string
+                }, {
+                    name: "Amount Of Winners:",
+                    value: options[1].value as string
+                }, {
+                    name: "Requirement(s):",
+                    value: options[2].value as string
+                }, {
+                    name: "Prize:",
+                    value: options[3].value as string
+                })
+                .setColor("#00ff00")
+                .setFooter("Thank you for your kind donation!!");
+            if (options[4]?.value) embed.addField("Message:", options[4].value as string);
+                
+            await interaction.reply(`<@&${client.config.ids.roles.giveaway}>`, {
+                embeds: [embed],
+                allowedMentions: { parse: ["roles", "users", "everyone"] }
+            });
+        } else {
+            const options = args[0].options as CommandInteractionOption[];
+
+            const embed: MessageEmbed = new MessageEmbed()
+                .setTitle(`${interaction.user.tag} wants to donate to a heist!!`)
+                .addField("Amount", options[0].value as string)
+                .setColor("#00ff00")
+                .setFooter("Thank you for your kind donation!!");
+            if (options[1]?.value) embed.addField("Message:", options[1].value as string);
+
+            await interaction.reply("<@&824272561009328140>", {
+                embeds: [embed],
+                allowedMentions: { parse: ["roles", "users", "everyone"] }
+            });
+        }
     }   
 });
