c894bcb72857d2d19e99e22bdb97ac1c0c53b735
code123456789101112 <73617040+code123456789101112@users.noreply.github.com>
2021-05-26 19:04:05 -0400

    Commit

--- src/slashCommands/editdono.ts
+++ src/slashCommands/editdono.ts
@@ -1,4 +1,5 @@
-import { CommandInteraction, CommandInteractionOption } from "discord.js";
+import { CommandInteraction, CommandInteractionOption, User } from "discord.js";
+import { DonationInstance } from "../interfaces/dbInterfaces.js";
 import Client from "../structs/client.js";
 import { SlashCommand } from "../structs/command.js";
 
@@ -13,11 +14,13 @@ export default new SlashCommand({
             options: [{
                 name: "user",
                 description: "The user to increase.",
-                type: "USER"
+                type: "USER",
+                required: true
             }, {
                 name: "amount",
                 description: "The amount to increase by.",
-                type: "INTEGER"
+                type: "INTEGER",
+                required: true
             }]
         }, {
             name: "decrease",
@@ -26,13 +29,16 @@ export default new SlashCommand({
             options: [{
                 name: "user",
                 description: "The user to decrease.",
-                type: "USER"
+                type: "USER",
+                required: true
             }, {
                 name: "amount",
                 description: "The amount to decrease by.",
-                type: "INTEGER"
+                type: "INTEGER",
+                required: true
             }]
-        }]
+        }],
+        defaultPermission: false
     },
     permissions: [{
         id: "824272561009328141",
@@ -40,7 +46,24 @@ export default new SlashCommand({
         permission: true
     }],
     async execute(client: Client, interaction: CommandInteraction, args: CommandInteractionOption[]) {
-        await interaction.reply("ok");
-        return { client, args };
+        const options: CommandInteractionOption[] = args[0].options as CommandInteractionOption[];
+        
+        const user: User = options[0].user as User;
+        let userDono: DonationInstance = await client.Donations.findOne({ where: { id: user.id } }) as DonationInstance;
+
+        if (!userDono) {
+            userDono = await client.Donations.create({
+                id: user.id,
+                amount: 0
+            });
+        }
+
+        if (args[0].name as string === "increase") {
+            await userDono.increment("amount", { by: options[1].value as number });
+            interaction.reply(`Successfully increased ${options[0].member.displayName}'s donation amount by ${options[1].value}.`);
+        } else {
+            await userDono.decrement("amount", { by: options[1].value as number });
+            interaction.reply(`Successfully decreased ${options[0].member.displayName}'s donation amount by ${options[1].value}.`);
+        }
     }
 });
